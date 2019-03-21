b3d1213dc973bafb2e96201e87ff44ed10c1ca51
Victor Tineo <victor.tineo@layerup.com.br>
2019-03-21 14:34:41 -0300

    [EDIT] Mobile das tres primeiras secoes

--- src/components/banner.js
+++ src/components/banner.js
@@ -7,17 +7,23 @@ import { Throttle } from 'react-throttle';
 
 
 const moveBaner = (elm) => {
+	// eixos de movimento do mouse
 	const x = elm.clientX;
 	const y = elm.clientY;
+	// elementos 
 	const pedreiro = document.getElementsByClassName('banner__pedreiro')[0]
 	const pedrasFrente = document.getElementsByClassName('banner__pedrasFrente')[0]
 	const pedrasFundo = document.getElementsByClassName('banner__pedrasFundo')[0]
 	const wrapper = document.getElementsByClassName('banner__wrapper')[0]
+	// tamanhos da tela
 	const wrapperWidth = wrapper.offsetWidth;
 	const wrapperHeight = wrapper.offsetHeight;
-	pedreiro.style.transform = `translate(${(x / wrapperWidth * 10) -4}px, ${(y / wrapperHeight * 10)-20}px)`
-	pedrasFrente.style.transform = `translate(${(x / wrapperWidth * 160) -4}px, ${(y / wrapperHeight * 160) -20}px)`
-	pedrasFundo.style.transform = `translate(${(x / wrapperWidth) -4}px, ${(y / wrapperHeight) -4}px)`
+	// alteração de estilo
+	console.log('eixo X: ',x)
+	console.log('tamanho do container: ', wrapperWidth)
+	console.log('resultado da divisão: ', x / wrapperWidth)
+	pedrasFrente.style.transform = `translate(${(x / wrapperWidth * 40) -4}px, ${(y / wrapperHeight * 40) -20}px)`
+	pedrasFundo.style.transform = `translate(${(x / wrapperWidth * 5) -4}px, ${(y / wrapperHeight * 5) -4}px)`
 }
 
 const Banner = () => {
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import './header.scss'
+import {FaBars} from 'react-icons/fa'
 
-const Header = () => {
-	return (
-		<header className="header">
-			<a href="/" className="header__logo">Mestre Construtor</a>
-			<nav className="header__items">
-				<a href="#" className="header__item">Benefícios</a>
-				<a href="#" className="header__item">Porque o Mestre Construtor</a>
-				<a href="#" className="header__item">Nossos vídeos</a>
-				<a href="#" className="header__item">Torne-se um mestre construtor</a>
-			</nav>
-		</header>
-	)
+class Header extends React.Component {
+	state = {
+		menuActive: false
+	}
+	toggleActive = () => {
+		this.setState(current => ({
+			menuActive: !current.menuActive
+		}))
+	}
+	render(){
+		return (
+			<header className="header">
+				<a href="/" className="header__logo">Mestre Construtor</a>
+				<nav className={`header__items ${this.state.menuActive === true ? 'header__items--active' : ''}`}>
+					<div className="header__itemsWrapper">
+						<a href="#" className="header__item">Benefícios</a>
+						<a href="#" className="header__item">Porque o Mestre Construtor</a>
+						<a href="#" className="header__item">Nossos vídeos</a>
+						<a href="#" className="header__item">Torne-se um mestre construtor</a>
+					</div>
+				</nav>
+				<span 
+					className="header__toggler"
+					onClick={this.toggleActive}
+					>
+					<FaBars />
+				</span>
+			</header>
+		)
+	}
 }
 
 export default Header
